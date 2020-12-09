366e44e8c026e70ebb3efbf87d370f307f5db060
svenson.e <svenson.ertel@gmail.com>
2020-12-09 10:29:15 +0800

    layout update

--- app.py
+++ app.py
@@ -53,11 +53,13 @@ CARD_TEXT_STYLE = {
 
 controls = dbc.FormGroup(
     [
-        html.P('Dropdown', style={
+        html.H2('Map', style=TEXT_STYLE),
+        html.Hr(),
+        html.P('Pick a Date', style={
             'textAlign': 'center'
         }),
         dcc.Dropdown(
-            id='dropdown',
+            id='dropdown_date',
             options=[{
                 'label': 'Value One',
                 'value': 'value1'
@@ -74,31 +76,22 @@ controls = dbc.FormGroup(
             multi=True
         ),
         html.Br(),
-        html.P('Range Slider', style={
-            'textAlign': 'center'
-        }),
-        dcc.RangeSlider(
-            id='range_slider',
-            min=0,
-            max=20,
-            step=0.5,
-            value=[5, 15]
-        ),
-        html.P('Check Box', style={
+        
+        html.P('Cases', style={
             'textAlign': 'center'
         }),
         dbc.Card([dbc.Checklist(
-            id='check_list',
+            id='check_list_cases',
             options=[{
-                'label': 'Value One',
+                'label': 'Deaths',
                 'value': 'value1'
             },
                 {
-                    'label': 'Value Two',
+                    'label': 'Recovered',
                     'value': 'value2'
                 },
                 {
-                    'label': 'Value Three',
+                    'label': 'Active Cases',
                     'value': 'value3'
                 }
             ],
@@ -106,29 +99,52 @@ controls = dbc.FormGroup(
             inline=True
         )]),
         html.Br(),
-        html.P('Radio Items', style={
+        html.H2('Bar Graph', style=TEXT_STYLE),
+        html.Hr(),
+        html.P('Pick a County', style={
             'textAlign': 'center'
         }),
-        dbc.Card([dbc.RadioItems(
-            id='radio_items',
+        dcc.Dropdown(
+            id='dropdown_county',
             options=[{
-                'label': 'Value One',
+                'label': 'Bali ',
                 'value': 'value1'
+            }, {
+                'label': 'Denpasar',
+                'value': 'value2'
             },
                 {
-                    'label': 'Value Two',
-                    'value': 'value2'
+                    'label': 'Badung',
+                    'value': 'value3'
                 },
                 {
-                    'label': 'Value Three',
+                    'label': 'Gianyiar',
+                    'value': 'value4'
+                },
+                {
+                    'label': 'Bangli',
+                    'value': 'value5'
+                },
+                {
+                    'label': 'Jembrana',
+                    'value': 'value6'
+                },
+                {
+                    'label': 'Buleleng',
+                    'value': 'value7'
+                },
+                {
+                    'label': 'Karangasem',
+                    'value': 'value8'
+                },
+                {
+                    'label': 'Foreigners',
                     'value': 'value3'
                 }
             ],
-            value='value1',
-            style={
-                'margin': 'auto'
-            }
-        )]),
+            value=['value1'],  # default value
+            multi=True
+        ),
         html.Br(),
         dbc.Button(
             id='submit_button',
@@ -155,7 +171,6 @@ content_first_row = dbc.Row([
     dbc.Col(
         dbc.Card(
             [
-
                 dbc.CardBody(
                     [
                         html.H4(id='card_title_1', children=['Card Title 1'], className='card-title',
@@ -182,46 +197,14 @@ content_first_row = dbc.Row([
         ),
         md=3
     ),
-    dbc.Col(
-        dbc.Card(
-            [
-                dbc.CardBody(
-                    [
-                        html.H4('Card Title 3', className='card-title', style=CARD_TEXT_STYLE),
-                        html.P('Sample text.', style=CARD_TEXT_STYLE),
-                    ]
-                ),
-            ]
 
-        ),
-        md=3
-    ),
-    dbc.Col(
-        dbc.Card(
-            [
-                dbc.CardBody(
-                    [
-                        html.H4('Card Title 4', className='card-title', style=CARD_TEXT_STYLE),
-                        html.P('Sample text.', style=CARD_TEXT_STYLE),
-                    ]
-                ),
-            ]
-        ),
-        md=3
-    )
 ])
 
 content_second_row = dbc.Row(
     [
         dbc.Col(
-            dcc.Graph(id='graph_1'), md=4
+            dcc.Graph(id='graph_1'), md=6
         ),
-        dbc.Col(
-            dcc.Graph(id='graph_2'), md=4
-        ),
-        dbc.Col(
-            dcc.Graph(id='graph_3'), md=4
-        )
     ]
 )
 
@@ -233,16 +216,6 @@ content_third_row = dbc.Row(
     ]
 )
 
-content_fourth_row = dbc.Row(
-    [
-        dbc.Col(
-            dcc.Graph(id='graph_5'), md=6
-        ),
-        dbc.Col(
-            dcc.Graph(id='graph_6'), md=6
-        )
-    ]
-)
 # Content Component
 content = html.Div(
     [
@@ -251,7 +224,6 @@ content = html.Div(
         content_first_row,
         content_second_row,
         content_third_row,
-        # content_fourth_row
     ],
     style=CONTENT_STYLE
 )
@@ -271,15 +243,13 @@ app.layout = html.Div([sidebar, content])
 @app.callback(
     Output('graph_1', 'figure'),
     [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
+    [State('check_list_cases', 'value'),
+    State('dropdown_county', 'value'),
      ])
-def update_graph_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+def update_graph_1(n_clicks, dropdown_county_value, check_list_value):
     print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
+    print(dropdown_county_value)
     print(check_list_value)
-    print(radio_items_value)
 
     # Bar Chart new and total cases per regency
     # path_data = r'C:\Users\ansve\Coding\Projects-DataScience\2020.12.05-Bali_Covid_Dash_App\Data'
@@ -294,56 +264,15 @@ def update_graph_1(n_clicks, dropdown_value, range_slider_value, check_list_valu
 
 
 @app.callback(
-    Output('graph_2', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_2(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)
-    fig = {
-        'data': [{
-            'x': [1, 2, 3],
-            'y': [3, 4, 5],
-            'type': 'bar'
-        }]
-    }
-    return fig
-
-
-@app.callback(
-    Output('graph_3', 'figure'),
-    [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
-     ])
-def update_graph_3(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-    print(n_clicks)
-    print(dropdown_value)
-    print(range_slider_value)
-    print(check_list_value)
-    print(radio_items_value)
-    df = px.data.iris()
-    fig = px.density_contour(df, x='sepal_width', y='sepal_length')
-    return fig
-
-
-@app.callback(
     Output('graph_4', 'figure'),
     [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
+    [State('dropdown_date', 'value'), State('check_list_cases', 'value'),
      ])
-def update_graph_4(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+def update_graph_4(n_clicks, dropdown_value, check_list_value):
     print(n_clicks)
     print(dropdown_value)
-    print(range_slider_value)
     print(check_list_value)
-    print(radio_items_value)  
+
     ## Bali Regency Covid Cases 
 
     # Sample data and figure
@@ -366,67 +295,32 @@ def update_graph_4(n_clicks, dropdown_value, range_slider_value, check_list_valu
     return fig
 
 
-# @app.callback(
-#     Output('graph_5', 'figure'),
-#     [Input('submit_button', 'n_clicks')],
-#     [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-#      State('radio_items', 'value')
-#      ])
-# def update_graph_5(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-#     print(n_clicks)
-#     print(dropdown_value)
-#     print(range_slider_value)
-#     print(check_list_value)
-#     print(radio_items_value)  # Sample data and figure
-#     df = px.data.iris()
-#     fig = px.scatter(df, x='sepal_width', y='sepal_length')
-#     return fig
-
-
-# @app.callback(
-#     Output('graph_6', 'figure'),
-#     [Input('submit_button', 'n_clicks')],
-#     [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-#      State('radio_items', 'value')
-#      ])
-# def update_graph_6(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
-#     print(n_clicks)
-#     print(dropdown_value)
-#     print(range_slider_value)
-#     print(check_list_value)
-#     print(radio_items_value)  # Sample data and figure
-#     df = px.data.tips()
-#     fig = px.bar(df, x='total_bill', y='day', orientation='h')
-#     return fig
-
 
 @app.callback(
     Output('card_title_1', 'children'),
     [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
+    [State('dropdown_date', 'value'), State('check_list_cases', 'value'),
+    State('dropdown_county', 'value'),
      ])
-def update_card_title_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+def update_card_title_1(n_clicks, dropdown_value, check_list_value, radio_items_value):
     print(n_clicks)
     print(dropdown_value)
-    print(range_slider_value)
     print(check_list_value)
-    print(radio_items_value)  # Sample data and figure
+    # Sample data and figure
     return 'Card Tile 1 change by call back'
 
 
 @app.callback(
     Output('card_text_1', 'children'),
     [Input('submit_button', 'n_clicks')],
-    [State('dropdown', 'value'), State('range_slider', 'value'), State('check_list', 'value'),
-     State('radio_items', 'value')
+    [State('dropdown_date', 'value'), State('check_list_cases', 'value'),
+    State('dropdown_county', 'value'),
      ])
-def update_card_text_1(n_clicks, dropdown_value, range_slider_value, check_list_value, radio_items_value):
+def update_card_text_1(n_clicks, dropdown_value, check_list_value, radio_items_value):
     print(n_clicks)
     print(dropdown_value)
-    print(range_slider_value)
     print(check_list_value)
-    print(radio_items_value)  # Sample data and figure
+    # Sample data and figure
     return 'Card text change by call back'
 
 
